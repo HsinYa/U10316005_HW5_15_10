5a0854d4ebe144718cdaf688eb4d4d9c85a0582e
HsinYa <starrysky012499@gmail.com>
2015-04-26 13:47:09 +0800

    Display the result about the deep copy
    Print the two situations differ by using the deep copy.

--- Test.java
+++ Test.java
@@ -21,5 +21,13 @@ public class Test {
 			myStack2.push(input.next());
 		}
 		
+		System.out.println("\nmyStack1 have " + myStack1.getSize() +" strings are: ");
+		for(int i=0; i<3; i++ ){
+			System.out.print(myStack1.get(i) + " ");
+		}
+		System.out.println("\nmyStack2 have " + myStack2.getSize() +" strings are: ");
+		for(int i=0; i<6; i++ ){
+			System.out.print(myStack2.get(i) + " ");
+		}
 	}
 }
